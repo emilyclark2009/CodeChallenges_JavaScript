e1ffccc2c34913bea101b248e1ebe0cbacebca28
Emily <heyemmers@gmail.com>
2022-08-05 11:06:17 -0400

    cleaned code

--- semiPrimeNumbers.js
+++ semiPrimeNumbers.js
@@ -7,13 +7,28 @@ For example, semiPrimeCount() called on 10 should return 3 since there are 3 sem
 
 Variations of this challenge were reported to have been asked at interviews with Google. If you’ve covered the material in Pass the Technical Interview with JavaScript or an equivalent, you should be able to solve this challenge. If you have trouble, try refreshing your knowledge with there first.
 */
+//function that determines whether or not a number is prime
+function isPrime(number) {
+    //to handle cases where the input is 1 or 0 becasue these special cases are not included in my algorithm
+    if (number === 1 || number === 0){
+        return false;
+    }
+        //If there are no factors of the number within the range 2 through the squareroot of the number, then the number is prime.
+    let upperLimit = Math.ceil(Math.sqrt(number));
+    for (let i=2; i <= upperLimit ; i++) {
+        if (number % i === 0) {
+            return false
+        }
+    return true
+    }
+}
 
 function semiPrimeCount(n) {
     let count = 0;
     const primes = [2];
     //finds all relevevant primes less than n that can potentially make a product with another prime less than n
-    const upperLimit = Math.ceil(n/2)
-    for (let i=3; i < upperLimit; i++) {
+    const upperLimit = Math.floor(n/2)
+    for (let i=3; i <= upperLimit; i++) {
         if (isPrime(i)) {
             primes.push(i);
         }
@@ -24,6 +39,8 @@ function semiPrimeCount(n) {
         for (let j of primes2) {
             if (k*j < n) {
                 count ++
+            }else{
+                break;
             }
         }
         primes2.shift();
@@ -31,31 +48,11 @@ function semiPrimeCount(n) {
     return count
 }
 
-console.log(semiPrimeCount(10));
+console.log(semiPrimeCount(13));
 
 // Leave this so we can test your code:
 module.exports = semiPrimeCount;
 
-//function that determines whether or not a number is prime
-function isPrime(number) {
-    // to handle cases where the input is a negative integer
-    number = Math.abs(number);
-    //to handle cases where the input is 1 or 0 becasue these special cases are not included in my algorithm
-    if (number === 1 || number === 0){
-        return false;
-    }
-    //to handle cases where the input is not an integer
-    if (Number.isInteger(number)) {
-        //If there are no factors of the number within the range 2 through the squareroot of the number, then the number is prime.
-        let upperLimit = Math.ceil(Math.sqrt(number));
-        for (let i=2; i <= upperLimit ; i++) {
-            if (number % i === 0) {
-                return false
-            }
-        return true
-        }
-    }else{
-        return "Number must be an integer to execute."
-    }
-}
+
+
 
